44ad242b34f3900d813c32f1a7fdda7ee0417d20
Asus <singhalokkumar427@gmail.com>
2025-05-17 16:42:17 +0530

    opposite triangle 2

--- src/Pattern_oppositeTriangleCounting.java
+++ src/Pattern_oppositeTriangleCounting.java
@@ -1,19 +1,26 @@
+//     1
+//    13
+//   135
+//  1357
+// 13579
+//1357911
 
 import java.util.Scanner;
-public class Pattern_oppositeTriangle2 {
+public class Pattern_oppositeTriangleCounting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length: ");
+        System.out.println("Enter the length of triangle: ");
         int a = sc.nextInt();
 
         for(int i=1; i<=a; i++){
-            for(int j=1; j<=a-i; j++){
-                System.out.print(" ");
+            for(int j=1; j<a-i; j++){
+                System.out.print("");
             }
             for(int j=1; j<=i; j++){
-                System.out.print(j);
+                System.out.print(2*j-1+" ");
             }
             System.out.println();
         }
+
     }
 }
