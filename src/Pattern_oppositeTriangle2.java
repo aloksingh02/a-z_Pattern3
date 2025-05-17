6adc034a7c094ac9a4e29c458db446a3857f7762
Asus <singhalokkumar427@gmail.com>
2025-05-17 16:08:43 +0530

    opposite triangle 2

---
+++ src/Pattern_oppositeTriangle2.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Pattern_oppositeTriangle2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length: ");
+        int a = sc.nextInt();
+
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=a-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+"");
+            }
+            System.out.println();
+        }
+    }
+}
