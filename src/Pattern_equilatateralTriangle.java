4bf02b91e3ca0bf41ca272af08d803e517d786a4
Asus <singhalokkumar427@gmail.com>
2025-05-28 15:10:58 +0530

    equilateral and rombus

--- src/Pattern_equilatateralTriangle.java
+++ src/Pattern_equilatateralTriangle.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-public class Pattern_rombus {
+public class Pattern_equilatateralTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the lenght: ");
+        System.out.print("Enter the length of triangle: ");
         int a = sc.nextInt();
 
         for(int i=1; i<=a; i++){
-            for(int j=1; j<a-i+1; j++){
+            for(int j=1; j<=a-i; j++){
                 System.out.print(" ");
             }
-
-            for(int j=1; j<=a; j++){
-                System.out.print("* ");
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print(j);
             }
+
             System.out.println();
         }
     }
